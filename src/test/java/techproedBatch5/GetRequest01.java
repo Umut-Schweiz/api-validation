223610e099831b61f2552384da04058547653b0c
Umut <av.umutgungor92@gmail.com>
2020-10-11 22:35:06 +0200

    Api-Validation

---
+++ src/test/java/techproedBatch5/GetRequest01.java
@@ -0,0 +1,79 @@
+package techproedBatch5;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+import static io.restassured.RestAssured.*;
+
+public class GetRequest01 {
+
+    //Rest-Assured kullnarak API Testing yapacagiz.
+    @Test
+
+    public void getMethod01(){
+
+        given().  //restAssured ten gelen bir method.
+                when(). //when den sonra --get, post, put, patch, delete-- methodları calistirilir.
+                //   get("https://restful-booker.herokuapp.com"). //get te sadece Endpoint kullanilir.
+                        get("https://restful-booker.herokuapp.com/booking").
+                then(). //dogrulama demek
+                assertThat().
+                statusCode(200);
+    }
+
+    //gete ile aldigimiz data yi console da gormek istiyoruz
+    //gelen datayi bir container icerisine alip yazdirmak gerekiyor. Response responnse
+    // kullanarak postman deki citinin aynıüisni elde edecegiz. Response body kısmını yazdirir.
+    @Test
+    public void getMethod02(){
+
+        Response response = given().
+                when().
+                get("https://restful-booker.herokuapp.com/booking");
+
+        //response body console yazdirmak icin
+        response.prettyPrint();
+
+        //Status code console yazdirmak icin.
+        System.out.println("Status Code: "+response.getStatusCode());
+
+        //Postmandaki status line consolda gormek icin
+        System.out.println("Status Line: " +response.getStatusLine());
+
+        //Header kismindaki content type yazdirilir.
+        // 1.) Yol:
+        System.out.println("Content Type: "+response.getContentType());
+        //2.) Yol:
+        System.out.println("Content Type -2"+response.header("Content-Type"));
+
+        //headerdaki tum bilgiler icin
+
+        System.out.println(response.getHeaders());
+
+        //headers ta spesific bilgi almak icin (date almak icin)
+        System.out.println(response.header("Date"));
+
+        response.
+                then().
+                assertThat().//hard asser hata buldugunda devam eder
+                statusCode(200).
+                contentType("application/json; charset=utf-8").
+                statusLine("HTTP/1.1 200 OK");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/test/java/techproedBatch5/GetRequest03.java
@@ -0,0 +1,54 @@
+package techproedBatch5;
+
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.*;
+
+public class GetRequest03 {
+
+        /*
+        Positive Scenario:
+         When Asagidaki Endpoint'e bir GET request yollayalim
+         https://restful-booker.herokuapp.com/booking/7
+         And Accept type “application/json” mi?
+         Then
+         HTTP Status Code 200
+         And Response format "application/json"
+         And firstname "Susan"
+         And lastname "Wilson"
+         And checkin date "2016-07-13"
+         And checkout date "2016-12-15"
+         */
+
+    @Test
+    public void get01() {
+        Response response = given().
+                accept("application/json").
+                when().
+                get(" https://restful-booker.herokuapp.com/booking/7");
+
+        response.prettyPrint();
+
+        //Status code 1. yol
+        response.
+                then().
+                assertThat().
+                statusCode(200).
+                contentType("application/json").
+                body("firstname", Matchers.equalTo("Sally")).
+                body("lastname", Matchers.equalTo("Wilson")).
+                body("totalprice", Matchers.equalTo(259)).
+                body("depositpaid", Matchers.equalTo(true)).
+                body("bookingdates.checkin", Matchers.equalTo("2015-03-12")).
+                body("bookingdates.checkout", Matchers.equalTo("2020-05-10"));
+        // body("additionalneeds",Matchers.equalTo("Breakfast"));
+
+
+        //Status code 2. yol
+        //   assertEquals(200,response.getStatusCode());
+
+    }
+}
+
